1ec25fbcc37c00f2b63a9b58f5c0370517f25135
uwishemajustine/alsen2 <alsenjastine90@gmail.com>
2019-07-26 16:48:33 +0200

    changing ids to classes in what we do

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
-    $("#Design").click(function(){
-      $("#pp").show();
-      $("#pp").hide();
+    $(".momo").click(function(){
+      $(".Design").show();
+      $(".momo").hide();
     });
     
   });
+  $(document).ready(function(){
+  $(".Design").click(function(){
+    $(".momo").show();
+    $(".Design").hide();
+  });
+});
+
+$(document).ready(function(){
+    $(".select").click(function(){
+      $(".Product-management").show();
+      $(".select").hide();
+    });
+    
+  });
+  $(document).ready(function(){
+  $(".Product-management").click(function(){
+    $(".select").show();
+    $(".Product-management").hide();
+  });
+});
+
+$(document).ready(function(){
+    $(".dev").click(function(){
+      $(".Development").show();
+      $(".dev").hide();
+    });
+    
+  });
+  $(document).ready(function(){
+  $(".Development").click(function(){
+    $(".dev").show();
+    $(".Development").hide();
+  });
+});
