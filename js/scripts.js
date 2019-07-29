781c92f15ae5d246ca351762202f5f727843893f
uwishemajustine/alsen2 <alsenjastine90@gmail.com>
2019-07-29 18:08:18 +0200

    adding some changes in the html,css,javascript

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,110 @@ $(document).ready(function() {
     $("#name").val()
     $("#email").val()
     $("#message").val()
-    alert("Your message have been received")
+    alert("Your message have been received thank you.")
   })
+});
+
+$(document).ready(function() {
+  $("#code1").hover(function(){
+$("#code1").mousemove(function(){
+   $("#folio1").show();
+  });
+});
+
+$("#code1").mouseleave(function(){
+  $("#folio1").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code2").hover(function(){
+$("#code2").mousemove(function(){
+   $("#folio2").show();
+  });
+});
+
+$("#code2").mouseleave(function(){
+  $("#folio2").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code3").hover(function(){
+$("#code3").mousemove(function(){
+   $("#folio3").show();
+  });
+});
+
+$("#code3").mouseleave(function(){
+  $("#folio3").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code4").hover(function(){
+$("#code4").mousemove(function(){
+   $("#folio4").show();
+  });
+});
+
+$("#code4").mouseleave(function(){
+  $("#folio4").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code5").hover(function(){
+$("#code5").mousemove(function(){
+   $("#folio5").show();
+  });
+});
+
+$("#code5").mouseleave(function(){
+  $("#folio5").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code6").hover(function(){
+$("#code6").mousemove(function(){
+   $("#folio6").show();
+  });
+});
+
+$("#code6").mouseleave(function(){
+  $("#folio6").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code7").hover(function(){
+$("#code7").mousemove(function(){
+   $("#folio7").show();
+  });
+});
+
+$("#code7").mouseleave(function(){
+  $("#folio7").hide();
+
+});
+});
+
+$(document).ready(function() {
+  $("#code8").hover(function(){
+$("#code8").mousemove(function(){
+   $("#folio8").show();
+  });
+});
+
+$("#code8").mouseleave(function(){
+  $("#folio8").hide();
+
+});
 });
